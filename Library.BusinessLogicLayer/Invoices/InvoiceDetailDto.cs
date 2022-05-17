882111d0b6af032d857df33e67035bba9a61c30e
pdong2001 <phamphuongdong2001@gmail.com>
2022-05-17 08:46:08 +0700

    commit

---
+++ Library.BusinessLogicLayer/Invoices/InvoiceDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using Library.BusinessLogicLayer.ProductDetails;
+using Library.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BusinessLogicLayer.Invoices
+{
+    public class InvoiceDetailDto : InvoiceDetail
+    {
+        public ProductDetailDto Product_Detail { get; set; }
+    }
+}
